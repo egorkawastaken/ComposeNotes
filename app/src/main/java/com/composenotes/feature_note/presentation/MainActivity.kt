e6e9e2985deefa3411228898776ea504294a079b
Egor <egordusik@gmail.com>
2021-12-22 23:24:22 +0300

    Init commit

---
+++ app/src/main/java/com/composenotes/feature_note/presentation/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.composenotes.feature_note.presentation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.composenotes.ui.theme.ComposeNotesTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeNotesTheme {
+
+            }
+        }
+    }
+}
